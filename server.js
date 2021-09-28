6151db69f52822b65a96d1dc7d5d28f256b9dbf9
Bashar-Owainat <owainatbashar13@gmail.com>
2021-09-28 21:50:58 +0300

    lab13

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose')
 const server = express();
 server.use(cors());
 const PORT = process.env.PORT;
+server.use(express.json());
 const bookModel = require('./modules/Books')
 
 mongoose.connect('mongodb://localhost:27017/Best-Books');
@@ -35,11 +36,12 @@ function seedBookInfo(){
 
 server.get('/', homeHandler);
 server.get('/getEmail', getEmail);
-
-
+server.post('./addBook', addBookHandler)
+server.delete('./deleteBook', deleteBookHandler)
 function homeHandler(req, res) {
     res.send("it's working")
 }
+
 
 //localhost:3001/getEmail?email=owainatbashar13@gmail.com
 function getEmail(req, res){
@@ -54,6 +56,47 @@ function getEmail(req, res){
         else{
             res.send(ownerData)
         }
+    })
+
+}
+//localhost:3001/addBook?title1=programming&description1=introToJava&email1=chance@gmail.com&status1=available
+
+//addBook
+async function addBookHandler (req, res){
+   
+    let { title1, description1,email1, status1} = req.body;
+    console.log(req.body);
+    await bookModel.create({
+        title: title1,
+        description: description1 ,
+        email: email1,
+         status: status1
+    })
+
+    bookModel.find({email: email1}, function (error, bookData){
+        if(error){
+            console.log('error in getting data', error)
+        }
+        else{
+            res.send(bookData)
+        }
+    })
+}
+
+function deleteBookHandler(res, req){
+    let bookID = req.query.bookID;
+    let ownerEmail = req.query.email;
+
+    bookModel.deleteOne({_id: bookID}).then(() =>{
+        bookModel.find({email: ownerEmail}, function (error, bookData){
+            if(error){
+                console.log('error in getting data', error)
+
+            }
+            else{
+                res.send(bookData)
+            }
+        })
     })
 
 }
