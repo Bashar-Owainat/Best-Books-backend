b1d767dd2df01db83728e9dc4f2f5bfcef03d607
Bashar-Owainat <owainatbashar13@gmail.com>
2021-09-29 15:16:33 +0300

    solved

--- server.js
+++ server.js
@@ -14,46 +14,46 @@ const bookModel = require('./modules/Books')
 mongoose.connect('mongodb://localhost:27017/Best-Books');
 
 
-function seedBookInfo(){
+function seedBookInfo() {
     const java = new bookModel({
-        title: 'java', 
-        description:'A book to learn java for beginners',
-        email: 'owainatbashar13@gmail.com', 
+        title: 'java',
+        description: 'A book to learn java for beginners',
+        email: 'owainatbashar13@gmail.com',
         status: 'available'
     })
     const js = new bookModel({
-        title: 'js', 
+        title: 'js',
         description: 'A book to learn javascript for begginers',
         email: 'hermen_the_mongoose@gmail.com',
-        status:'not available'
+        status: 'not available'
     })
 
     java.save();
     js.save();
 
 }
- //  seedBookInfo();
+//  seedBookInfo();
 
 server.get('/', homeHandler);
 server.get('/getEmail', getEmail);
-server.post('./addBook', addBookHandler)
-server.delete('./deleteBook', deleteBookHandler)
+server.post('/addBook', addBookHandler)
+server.delete('/deleteBook', deleteBookHandler)
 function homeHandler(req, res) {
     res.send("it's working")
 }
 
 
 //localhost:3001/getEmail?email=owainatbashar13@gmail.com
-function getEmail(req, res){
+function getEmail(req, res) {
 
     let email = req.query.email;
     console.log(email);
-    
-    bookModel.find({email:email}, function(error,ownerData) {
-        if(error){
+
+    bookModel.find({ email: email }, function (error, ownerData) {
+        if (error) {
             console.log('error in getting data', error)
         }
-        else{
+        else {
             res.send(ownerData)
         }
     })
@@ -62,38 +62,42 @@ function getEmail(req, res){
 //localhost:3001/addBook?title1=programming&description1=introToJava&email1=chance@gmail.com&status1=available
 
 //addBook
-async function addBookHandler (req, res){
-   
-    let { title1, description1,email1, status1} = req.body;
+async function addBookHandler(req, res) {
+    console.log('data test')
+    let { title1, description1, email1, status1 } = req.body;
     console.log(req.body);
-    await bookModel.create({
+    bookModel.create({
         title: title1,
-        description: description1 ,
+        description: description1,
         email: email1,
-         status: status1
+        status: status1
+    }).then(() => {
+        console.log("testtt");
+        bookModel.find({ email: email1 }, function (error, bookData) {
+            if (error) {
+                console.log('error in getting data', error)
+            }
+            else {
+                res.send(bookData)
+            }
+        })
     })
 
-    bookModel.find({email: email1}, function (error, bookData){
-        if(error){
-            console.log('error in getting data', error)
-        }
-        else{
-            res.send(bookData)
-        }
-    })
+
 }
 
-function deleteBookHandler(res, req){
+function deleteBookHandler(req,res ) {
+    console.log(req.query)
     let bookID = req.query.bookID;
-    let ownerEmail = req.query.email;
+    let ownerEmail = req.query.ownerEmail;
 
-    bookModel.deleteOne({_id: bookID}).then(() =>{
-        bookModel.find({email: ownerEmail}, function (error, bookData){
-            if(error){
+    bookModel.deleteOne({ _id: bookID }).then(() => {
+        bookModel.find({ email: ownerEmail }, function (error, bookData) {
+            if (error) {
                 console.log('error in getting data', error)
 
             }
-            else{
+            else {
                 res.send(bookData)
             }
         })
